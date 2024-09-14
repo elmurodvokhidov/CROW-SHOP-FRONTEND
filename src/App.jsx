7079b97077797f6c7334942dd6f53c825a230f00
ilhomjonxolmirzayevv <joha21048@gmail.com>
2024-09-14 17:01:29 +0500

    C:/Program Files/Git/login olib tashlandi

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
-import Login from "./pages/Login";
 
 export default function App() {
   return (
@@ -11,7 +10,6 @@ export default function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
 
       <Footer />
